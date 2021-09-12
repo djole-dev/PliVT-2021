47a043464e98d3c53d2440422087d85d6d1d795f
djole-dev <djsuboticdev@gmail.com>
2021-09-12 22:52:17 +0200

    Initialize react project

---
+++ 04-front-end/src/components/Application/Application.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Application.css';
+
+function Application() {
+  return (
+    <div className="Application">
+    ...
+    </div>
+  );
+}
+
+export default Application;
